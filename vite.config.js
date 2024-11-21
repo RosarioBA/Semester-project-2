026405c3cd8640550371121bde0fe91f1535d02d
RosarioBA <146356539+RosarioBA@users.noreply.github.com>
2024-11-21 15:05:00 +0100

    feat: add listing pages

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { list } from 'postcss';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -9,7 +10,10 @@ export default defineConfig({
         main: 'index.html',
         login: 'pages/login.html',
         register: 'pages/register.html',
-        profile: 'pages/profile.html'
+        profile: 'pages/profile.html',
+        listing: 'pages/listing.html',
+        singlelisting: 'pages/single-listing.html',
+        createlistings: 'pages/create-listing.html'
       }
     }
   }
